b780b0a21782bd15211c1b8afa215916e4b5d083
Andrew Rodgers <acedrew@acedrew.com>
2016-06-21 16:47:20 -0400

    Started basic skeleton

---
+++ TelemeterRC.py
@@ -0,0 +1,15 @@
+import socket
+
+class TelemeterRcDaemon():
+    """docstring for TelemeterRcDaemon"""
+    def __init__(self, src, dst, type):
+        self.src = src
+        self.dst = dst
+        self.type = type
+        self.socket = socket.socket(AF_PACKET, SOCK_RAW)
+        self.socket.bind((interface, 0))
+
+    def sendFrame(self, payload):
+            assert(len(self.src) == len(self.dst) == 6)
+            assert(len(self.type) == 2)
+            return self.socket.send(self.src + self.dst + self.type + payload)
