6f65b91570f1e3fdccf31aaca9d224b1412f037a
Andrew Rodgers <acedrew@acedrew.com>
2016-06-22 11:05:44 -0400

    python3
    Added Python3 (I think)

--- appcodec.py
+++ appcodec.py
@@ -1,8 +1,9 @@
 def decode(payload):
     """do stuff with frame, generate output"""
-    print(payload)
+    print(payload.decode('utf-8'))
 
 def encode():
     """encode payload from sensors"""
-    payload == bytes("blablablabalabala")
+
+    payload == "blablablabalabala".encode("utf-8")
     return payload
